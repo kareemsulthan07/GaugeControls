198cb37bec79d39bee5eb4499a5654924ac83cc8
Kareem Sulthan <itsmystyle.shaik@hotmail.com>
2021-01-22 13:25:54 +0530

    GaugeControl2

---
+++ GaugeControl2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GaugeControl2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void valueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                fillPathRT.Angle = valueSlider.Value;
+
+                var angle = 180 + valueSlider.Value;
+                double radius = 300 - 12.5, centerX = needleLine.X1, centerY = needleLine.Y1;
+
+                double x2 = (radius * Math.Cos(angle * Math.PI / 180)) + centerX;
+                double y2 = (radius * Math.Sin(angle * Math.PI / 180)) + centerY;
+
+                needleLine.X2 = x2;
+                needleLine.Y2 = y2;
+
+                Canvas.SetLeft(needleEllipse, x2 - 12.5);
+                Canvas.SetTop(needleEllipse, y2 - 12.5);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
